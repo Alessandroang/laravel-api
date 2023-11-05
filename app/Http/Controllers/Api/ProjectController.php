6a84267e8ddfb680e4407b11aec98f2513354825
Alessandroang <133670463+Alessandroang@users.noreply.github.com>
2023-11-05 21:43:19 +0100

    Add milestone 1,2

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Project::paginate(10);
+        return response()->json($projects);
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+}
